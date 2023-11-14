1b3000c368f570ae3e4d67477927de02d11d53fe
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2023-11-14 14:55:04 -0600

    Edited profile css

--- Frontend/src/Components/Buttons/Tweet/TweetButton.tsx
+++ Frontend/src/Components/Buttons/Tweet/TweetButton.tsx
@@ -1,39 +1,17 @@
-import './Button.css'
+import './TweetButton.css'
 import { useNavigate } from "react-router-dom";
-import { setAuthToken } from '../../../API/BaseAPI';
-import { useState } from 'react';
-import { message, notification } from 'antd'
-import { CloseOutlined } from '@ant-design/icons';
+import {  notification } from 'antd'
 
 
 interface buttonProps {
     disable: boolean
     placeHolder: string;
-    page: string;
-    type: number;
-    user?: string;
-    name?: string;
-    password?: string;
 }
-//type:
-//1 login (Call EP)
-//2 navigate create account
-//3 create account (Call EP)
 
-const Button = ({ disable, placeHolder, page, type, user, name, password }: buttonProps) => {
+const Button = ({ disable, placeHolder}: buttonProps) => {
     //cosntants
-    const [invalidLogin, setInvalidLogin] = useState<boolean>(false);
-    const navigate = useNavigate();
-    const [api, contextHolder] = notification.useNotification();
 
-    const openNotification = (title: string, message: string) => {
-        api.open({
-            message: title,
-            description:message,
-            duration: 2.0,
 
-        });
-    };
 
     const handleButton = async () => {
 
@@ -41,7 +19,6 @@ const Button = ({ disable, placeHolder, page, type, user, name, password }: butt
 
     return (
         <>
-            {contextHolder}
             <div className="button-container">
                 <button className="button-icon" disabled={disable} onClick={handleButton}>{placeHolder}</button>
             </div>
