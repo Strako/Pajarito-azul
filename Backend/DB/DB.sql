7b88cf1d7e5d8303da701459c5a5089c5a5a4124
JocznHM <42367132+JocznHM@users.noreply.github.com>
2023-11-16 13:01:25 -0600

    Update DB.sql

--- Backend/DB/DB.sql
+++ Backend/DB/DB.sql
@@ -41,7 +41,7 @@ DROP TABLE IF EXISTS `tweets`;
 CREATE  TABLE IF NOT EXISTS `tweets` (
   `tweetid` INT NOT NULL AUTO_INCREMENT ,
   `userid` INT NOT NULL ,
-  `description` VARCHAR(140) NOT NULL ,
+  `description` VARCHAR(255) NOT NULL ,
   `tweetImage` VARCHAR(90) NULL ,
   `datetime` VARCHAR(30) NOT NULL ,
   PRIMARY KEY (`tweetid`) ,
@@ -63,6 +63,20 @@ CREATE TABLE IF NOT EXISTS `likes` (
   CONSTRAINT `fk_likes_tweet` FOREIGN KEY (`tweetid` ) REFERENCES `tweets` (`tweetid` ));
 
 /*
+  Table structure for comments
+  atributte: commentid, userid, tweetid, date, comment
+*/
+CREATE TABLE IF NOT EXISTS `comments` (
+  `commentid` INT NOT NULL AUTO_INCREMENT,
+  `userid` INT NOT NULL ,
+  `tweetid` INT NOT NULL ,
+  `comment` VARCHAR(255) NOT NULL ,
+  `date` VARCHAR(15) NULL,
+  PRIMARY KEY (`commentid`) ,
+  FOREIGN KEY (`userid` ) REFERENCES `users` (`userid` ),
+  CONSTRAINT `fk_comments_tweet` FOREIGN KEY (`tweetid` ) REFERENCES `tweets` (`tweetid` ));
+
+/*
   Table structure for table follows
   atributte: followid, followerid, followingid, date
 */
@@ -75,3 +89,62 @@ CREATE TABLE IF NOT EXISTS `follows`(
   PRIMARY KEY (`followid`) ,
   FOREIGN KEY (`followerid` ) REFERENCES `users` (`userid` ),
   FOREIGN KEY (`followingid` ) REFERENCES `users` (`userid` ));
+
+/*
+  Table structure for table notifications
+  atributte: notificationid, emitterid, receiverid, date, action, status
+*/
+DROP TABLE IF EXISTS `notifications`;
+CREATE TABLE IF NOT EXISTS `notifications`(
+  `notificationid` INT NOT NULL AUTO_INCREMENT ,
+  `emitterid` INT NOT NULL ,
+  `receiverid` INT NOT NULL,
+  `date` VARCHAR(15) NOT NULL,
+  `action` VARCHAR(15) NOT NULL,
+  `status` TINYINT(1) DEFAULT 0,
+  PRIMARY KEY (`notificationid`) ,
+  FOREIGN KEY (`emitterid` ) REFERENCES `users` (`userid` ),
+  FOREIGN KEY (`receiverid` ) REFERENCES `users` (`userid` ));
+
+/*
+  Trigger for the table tweets
+*/
+
+DELIMITER //
+CREATE TRIGGER after_insert_like
+AFTER INSERT ON likes
+FOR EACH ROW
+BEGIN
+  INSERT INTO notifications (emitterid, receiverid, date, action, status)
+  VALUES (NEW.userid, (SELECT userid FROM tweets WHERE tweetid = NEW.tweetid), NOW(), 'like', 0);
+END;
+//
+DELIMITER ;
+
+/*
+  -- Trigger for the table follows
+*/
+DELIMITER //
+CREATE TRIGGER after_insert_follow
+AFTER INSERT ON follows
+FOR EACH ROW
+BEGIN
+  INSERT INTO notifications (emitterid, receiverid, date, action, status)
+  VALUES (NEW.followerid, NEW.followingid, NOW(), 'follow', 0);
+END;
+//
+DELIMITER ;
+
+/*
+  -- Trigger for the table comments
+*/
+DELIMITER //
+CREATE TRIGGER after_insert_comment
+AFTER INSERT ON comments
+FOR EACH ROW
+BEGIN
+  INSERT INTO notifications (emitterid, receiverid, date, action, status)
+  VALUES (NEW.userid, (SELECT userid FROM tweets WHERE tweetid = NEW.tweetid), NOW(), 'comment', 0);
+END;
+//
+DELIMITER ;
